933d10cbbe3df1cb58d135232af323db8a3f7cff
fisenko-gb <fisenko-gb@yandex.ru>
2023-03-10 00:24:41 +0400

    перомидальная сортировка

---
+++ Seminars/Seminar02/src/Main.java
@@ -0,0 +1,65 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = generateRandomArray(10);
+        System.out.println("Исходный массив: ");
+        printArray(array);
+        sort(array);
+        System.out.println("Отсортированный массив: ");
+        printArray(array);
+    }
+
+    public static int[] generateRandomArray(int size) {
+        int[] array = new int[size];
+        Random random = new Random();
+        for (int i = 0; i < size; i++) {
+            array[i] = random.nextInt(size); // генерируем случайное число от 0 до 99
+        }
+        return array;
+    }
+
+    public static void sort(int arr[]) {
+        int n = arr.length;
+
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+
+        for (int i = n - 1; i >= 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            heapify(arr, i, 0);
+        }
+    }
+
+    public static void heapify(int arr[], int n, int i) {
+        int largest = i;
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
+
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            heapify(arr, n, largest);
+        }
+    }
+
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+}
+
